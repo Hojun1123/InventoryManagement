880fb6ba7a23e22560cb6056949e7641c6f77378
동민 <104581585+krcut@users.noreply.github.com>
2022-08-01 21:21:20 +0900

    메인페이지/로그인페이지 절대좌표 마구잡이로 입력되어 있음 css,js도 안쓰는 파일들 많음 수정 예정

--- app.py
+++ app.py
@@ -1,53 +1,15 @@
-from flask import Flask
-from flask import request, render_template, redirect
-import dbController as dc
-import datetime
-# Flask 객체 생성
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-# 데코레이션 테스트
-@app.route('/test')
-def test():
-    return "test print"
-
-
-# 인덱스 페이지
 @app.route('/')
-def index():  # put application's code here
-    return render_template("index.html")
+def login():
+    return render_template('LOGIN.html')
 
+@app.route('/main')
+def main():
+    return render_template('mainpage.html')
 
-# 입고(바코드 입력 페이지)
-@app.route('/inputrawbarcodestring')
-def inputrawbarcodestring():  # put application's code here
-    data = dc.getallrecentrawbarcodes()
-    return render_template("./main/inputRawBarcodeString.html", data=data)
-
-
-# 바코드 읽기
-@app.route('/readBarcode', methods=['GET', 'POST'])
-def readbarcode():
-    if request.method == 'GET':
-        # GET으로 들어올때
-        data = dc.getallrecentrawbarcodes()
-        return render_template("./main/inputRawBarcodeString.html", data=data)
-    else:
-        # GET이 아닌 request
-        rawBarcodeData = request.form.get("barcode")
-        #locationData = request.form.get("location")
-        if rawBarcodeData != "":
-            tm = datetime.datetime.now()
-            # time부분 나중에 함수로 빼기
-            dc.appendrawbarcode([rawBarcodeData[6:12], rawBarcodeData, tm.strftime("%Y%m%d"), tm.strftime("%X")])
-        data = dc.getallrecentrawbarcodes()
-        return render_template("./main/inputRawBarcodeString.html", data=data)
-
-
-# flask 구동 (main)
 if __name__ == '__main__':
-    # hp 지정
-    # app.run(host="127.0.0.1", port=5000, debug=True)
-    # 49.174.54.239:9375
-    #
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
